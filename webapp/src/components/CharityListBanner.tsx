d88ef7183dcf87266bd998bfb21db6e07e8e33b8
Alfie Lamerton <46544750+alamerton@users.noreply.github.com>
2023-04-24 09:42:36 +0100

    Added new banners

---
+++ webapp/src/components/CharityListBanner.tsx
@@ -0,0 +1,12 @@
+import { Box, Typography } from "@mui/material";
+import UserProfile from "../helper/UserProfile";
+
+const CharityListBanner = () => {
+  return (
+    <Box sx={{ padding: "2rem", margin: "auto", width: "80%" }}>
+      <Typography variant="h3">Welcome</Typography>
+    </Box>
+  );
+};
+
+export default CharityListBanner;
