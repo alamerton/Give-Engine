ff264d82706a99be3382e7f6d35f414a21a99045
Alfie Lamerton <46544750+alamerton@users.noreply.github.com>
2023-04-26 09:41:48 +0100

    committing again to update files

---
+++ webapp/src/components/RecommendationList.tsx
@@ -0,0 +1,75 @@
+import { Box, Button, Card, List, Typography } from "@mui/material";
+import StarIcon from "@mui/icons-material/Star";
+import StarBorderIcon from "@mui/icons-material/StarBorder";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import titleCase from "../helper/TitleCapitalisation";
+
+interface ICharity {
+  id: number;
+  name: string;
+  url: string;
+}
+
+const RecommendationList: React.FC = () => {
+  const [charities, setCharities] = useState<ICharity[]>([]);
+
+  useEffect(() => {
+    const getCharities = async () => {
+      const response = await axios.get<{ charities: ICharity[] }>(
+        "http://localhost:5000/charities" // calls flask api
+      );
+      setCharities(response.data.charities);
+    };
+    getCharities();
+  }, []);
+
+  return (
+    <Box
+      sx={{
+        width: "80%",
+        margin: "auto",
+      }}
+    >
+      <List>
+        {charities.map((charity) => (
+          <Card
+            key={charity.id}
+            sx={{
+              padding: "2rem",
+              margin: "1rem",
+              display: "flex",
+            }}
+          >
+            <Box>
+              <Typography variant="h5">{titleCase(charity.name)}</Typography>
+              {/* Icon row goes here, number of grey icons depends on rating
+                create an enumeration for each rating such as: 1 = 1 star 4 star borders, 3.5 = 3 stars 1 starhalf 1 star border */}
+              {/* Logic like 'if charity has rating...' to do the star system. For now the stars are just there for show */}
+              <Box sx={{ margin: "auto", display: "flex" }}>
+                <StarIcon sx={{ color: "green" }} />
+                <StarIcon sx={{ color: "green" }} />
+                <StarIcon sx={{ color: "green" }} />
+                <StarIcon sx={{ color: "green" }} />
+                <StarBorderIcon sx={{ color: "green" }} />
+              </Box>
+            </Box>
+            <Box
+              sx={{
+                marginLeft: "auto",
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              <Button variant="contained" href={charity.url}>
+                Donate
+              </Button>
+            </Box>
+          </Card>
+        ))}
+      </List>
+    </Box>
+  );
+};
+
+export default RecommendationList;
