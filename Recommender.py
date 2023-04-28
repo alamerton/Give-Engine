029c3836167355ccadd01015b8762ac5465771a7
Alfie Lamerton <46544750+alamerton@users.noreply.github.com>
2023-04-28 14:43:40 +0100

    Updated getRecommendations to return a list of charity ids

--- Recommender.py
+++ Recommender.py
@@ -30,49 +30,32 @@ count_matrix = countVector.fit_transform(
 
 # dataFrame2 = dataFrame2.reset_index()
 
-# print("data frame 2", dataFrame2)
-
+# indices = pd.Series(dataFrame2.index, index=dataFrame2['name'])
 indices = pd.Series(dataFrame2.index, index=dataFrame2['name'])
 
+# all_names = [dataFrame2['name'][i] for i in range(len(dataFrame2['name']))]
 all_names = [dataFrame2['name'][i] for i in range(len(dataFrame2['name']))]
 
 
 def get_recommendations(name):
-    # start = time.time()
     cosSim = cosine_similarity(count_matrix, count_matrix)
     idx = indices[name]
-    # returns a list of charity indexes, with their similarity score to the input charity
     sim_scores = list(enumerate(cosSim[idx]))
-    # returns sorted version of the list, starting with the most similar charity
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-    # reduces the list into just 10 charities
     sim_scores = sim_scores[1: 11]
-    # saves the ids of the most similar charities into a new list
     charity_indices = [i[0] for i in sim_scores]
-    # creates a dataframe containing the ids and names of the charities
-    # displayName = dataFrame2['name'].iloc[charity_indices]
-    # # creates a new dataframe with a 'name' column
-    # return_dataFrame = pd.DataFrame(columns=['charities'])
-    # print("return dataframe", return_dataFrame)
-    # return_dataFrame['name'] = displayName
-    # end = time.time()
-    # print(end-start)
-    # return return_dataFrame
-    return charity_indices
+    getId = dataFrame2['id'].iloc[charity_indices]
+    return_dataFrame = pd.DataFrame(columns=['id'])
+    return_dataFrame['id'] = getId
+    top10IDs = return_dataFrame['id'].values.tolist()
+    return top10IDs
 
 
 @app.route("/", methods=['POST'])
 def apiResponse():
     charityName = request.get_json()['charityName']
     recommendedCharities = get_recommendations(charityName)
-    print("recommended charities", recommendedCharities)
-    # responseObject = recommendedCharities.to_json()
-    # responseObject = {
-    #     "charities": recommendedCharities
-    # }
     responseObject = json.dumps(recommendedCharities)
-    print(responseObject)
-    # response.headers.add('Access-Control-Allow-Origin', '*')
     return Response(responseObject, status=200, mimetype='application/json')
 
 
