b29fa98f11152c0da20bf2682e97bbe606d13c31
Alfie Lamerton <46544750+alamerton@users.noreply.github.com>
2023-04-28 10:10:48 +0100

    Added Charity model class

---
+++ src/models/Charity.ts
@@ -0,0 +1,26 @@
+class Charity {
+  id: string;
+  name: string;
+  url: string;
+
+  constructor(id: string, name: string, url: string) {
+    this.id = id;
+    this.name = name;
+    this.url = url;
+  }
+  static getAll(
+    callback: (error: Error | null, charities: Charity[] | null) => void
+  ) {
+    connection.query("SELECT * FROM charities", (error, results) => {
+      if (error) {
+        callback(error, null);
+      } else {
+        const charities = results.map(
+          (result: any) => new Charity(result.id, result.name, result.url)
+        );
+        callback(null, charities);
+      }
+    });
+  }
+}
+export default Charity;
