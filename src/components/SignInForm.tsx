4852194e34e84715255e738c561f5bd170a243a1
Alfie Lamerton <46544750+alamerton@users.noreply.github.com>
2023-05-15 11:31:22 +0100

    Removed users directory from sign in url

--- src/components/SignInForm.tsx
+++ src/components/SignInForm.tsx
@@ -15,7 +15,7 @@ export default function SignInForm() {
   const navigate = useNavigate();
   async function handleSubmit(email: string, password: string) {
     await axios
-      .post("http://localhost:5001/users/signin", {
+      .post("http://localhost:5001/signin", {
         email: email,
         password: password,
       })
