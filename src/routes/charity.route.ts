35747a7fbcb5dee22147d97bdf2d233ae3fba190
Alfie Lamerton <46544750+alamerton@users.noreply.github.com>
2023-07-04 23:22:22 +0100

    added like routes

--- src/routes/charity.route.ts
+++ src/routes/charity.route.ts
@@ -6,4 +6,8 @@ const router = Router();
 router.get("/charities", CharityController.getAll);
 router.get("/charities/:id", CharityController.getCharity);
 
+router.post("/likes", LikeController.add);
+router.post("/likes/getLikeByUserId", LikeController.getLikeByUserId);
+router.delete("/likes", LikeController.remove);
+
 export default router;
