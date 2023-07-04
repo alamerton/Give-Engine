ae48ae01bc0e6776d712e79f8946fbe7541f73f1
Alfie Lamerton <46544750+alamerton@users.noreply.github.com>
2023-07-04 23:31:34 +0100

    Copied like controller and Like model over and imported like controller

--- src/routes/charity.route.ts
+++ src/routes/charity.route.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import CharityController from "../controllers/charity.controller";
+import LikeController from "../controllers/like.controller";
 
 const router = Router();
 
 router.get("/charities", CharityController.getAll);
 router.get("/charities/:id", CharityController.getCharity);
-
 router.post("/likes", LikeController.add);
 router.post("/likes/getLikeByUserId", LikeController.getLikeByUserId);
 router.delete("/likes", LikeController.remove);
