4b8811bff4a4931052f65c7535d7da74441030b6
Alfie Lamerton <46544750+alamerton@users.noreply.github.com>
2023-07-05 13:12:21 +0100

    added likes unit test

--- src/routes/charity.route.ts
+++ src/routes/charity.route.ts
@@ -3,11 +3,13 @@ import CharityController from "../controllers/charity.controller";
 import LikeController from "../controllers/like.controller";
 
 const router = Router();
-
+// Charities
 router.get("/charities", CharityController.getAll);
 router.get("/charities/:id", CharityController.getCharity);
+// Likes
 router.post("/likes", LikeController.add);
 router.post("/likes/getLikeByUserId", LikeController.getLikeByUserId);
 router.delete("/likes", LikeController.remove);
+//
 
 export default router;
--- tests/server.u.spec.ts
+++ tests/server.u.spec.ts
@@ -446,5 +446,50 @@ describe("Test Charities API Express Server", () => {
   });
 });
 
+describe("Test Likes API Express Server", () => {
+  test("the '/' endpoint with a POST request to add a new like", async () => {
+    // arrange
+    const like = {
+      id: "1",
+      userId: "2",
+      charityId: "3",
+      dateLiked: "14-07-2022",
+    };
+    const expectedStatusCode = 201;
+    // act
+    const response = await request(server).post("/").send(like);
+    // assert
+    expect(response.status).toEqual(expectedStatusCode);
+  });
+  test("the '/getLikeByUserId' endpoint with a POST request containing an existing user id", async () => {
+    // arrange
+    const userId = "cef9a9b6-676e-4017-aa64-e87627ea0748";
+    const requestObject = {
+      userId: userId,
+    };
+    const expectedStatusCode = 201;
+    // act
+    const response = await request(server)
+      .post("/getLikeByUserId")
+      .send(requestObject);
+    // assert
+    expect(response.status).toEqual(expectedStatusCode);
+  });
+  test("the '/getLikeByUserId' endpoint with a POST request containing an unknown user id", async () => {
+    // arrange
+    const userId = "abababab-aaaa-bbbb-aaaa-bbbbbbaaaaaa";
+    const requestObject = {
+      userId: userId,
+    };
+    const expectedStatusCode = 404;
+    // act
+    const response = await request(server)
+      .post("/getLikeByUserId")
+      .send(requestObject);
+    // assert
+    expect(response.status).toEqual(expectedStatusCode);
+  });
+});
+
 // incorrect http request
 // improper id
